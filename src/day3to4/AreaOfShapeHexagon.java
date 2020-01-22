3a771ab9e63108bf3a8b6bb8f0a2e707af0583c6
841423 <841423@PCIN485224.cts.com>
2020-01-22 10:19:19 +0530

    Done

--- src/day3to4/AreaOfShapeHexagon.java
+++ src/day3to4/AreaOfShapeHexagon.java
@@ -1,125 +1,125 @@
-//package day3to4;
-//
-//import java.util.Scanner;
-//
-//public class AreaOfShapeHexagon {
-//
-//	public static void main(String[] args) {
-//		Scanner sc=new Scanner(System.in);
-//		System.out.println("1. Rectangle \r\n" + 
-//				"2. Square \r\n" + 
-//				"3. Circle \r\n" + 
-//				"4. Hexagon \r\n" + 
-//				"Area Calculator --- Choose your shape \r\n" + 
-//				"");
-//		int choice=sc.nextInt();
-//		if(choice==1) {
-//			System.out.println("Enter length and breadth: ");
-//			int length=sc.nextInt();
-//			int breadth=sc.nextInt();
-//			Rectangle r=new Rectangle(length, breadth);
-//			System.out.println("Area of Rectangle is: "+Math.round(r.calculateArea()* 100.0)/100.0);
-//		}else if(choice==2) {
-//			System.out.println("Enter side: ");
-//			int side=sc.nextInt();
-//			Square s=new Square(side);
-//			System.out.println("Area of Square is: "+Math.round(s.calculateArea()* 100.0)/100.0);
-//		}else if(choice==3){
-//			System.out.println("Enter Radius: ");
-//			int radius=sc.nextInt();
-//			Circle c=new Circle(radius);
-//			System.out.println("Area of Circle is: "+Math.round(c.calculateArea()* 100.0)/100.0);
-//		
-//		}else {
-//			System.out.println("Enter side: ");
-//			int side=sc.nextInt();
-//			System.out.println("Area of Hexagon is: 0.00");
-//		}
-//		sc.close();
-//
-//
-//	}
-//
-//}
-//class Shape{
-//	protected String shapeName; 
-//	double calculateArea() {
-//		return 0;
-//	}
-//	public Shape(String shapeName) {
-//		this.shapeName = shapeName;
-//	}
-//}
-//class Square extends Shape{
-//	int side;
-//	double calculateArea() {
-//		return side*side;
-//	}
-//	public Square( int side) {
-//		super("Square");
-//		this.side = side;
-//	}
-//	public int getSide() {
-//		return side;
-//	}
-//	public void setSide(int side) {
-//		this.side = side;
-//	}
-//	
-//	
-//}
-//class Rectangle extends Shape{
-//	int length;
-//	int breadth;
-//	double calculateArea() {
-//		return length*breadth;
-//	}
-//	public Rectangle(int length, int breadth) {
-//		super("Rectangle");
-//		this.length = length;
-//		this.breadth = breadth;
-//	}
-//	public int getLength() {
-//		return length;
-//	}
-//	public void setLength(int length) {
-//		this.length = length;
-//	}
-//	public int getBreadth() {
-//		return breadth;
-//	}
-//	public void setBreadth(int breadth) {
-//		this.breadth = breadth;
-//	}
-//	
-//	
-//}
-//class Circle extends Shape{
-//	int radius;
-//	double calculateArea() {
-//		return Math.PI*(radius*radius);
-//	}
-//	public Circle( int radius) {
-//		super("Circle");
-//		this.radius = radius;
-//	}
-//	public int getRadius() {
-//		return radius;
-//	}
-//	public void setRadius(int radius) {
-//		this.radius = radius;
-//	}
-//	
-//	
-//	
-//}
-//
-//class Hexagon extends Shape{
-//	int side;
-//
-//	public Hexagon( int side) {
-//		super("Hexagon");
-//		this.side = side;
-//	}
-//	
-//}
+package day3to4;
+
+import java.util.Scanner;
+
+public class AreaOfShapeHexagon {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("1. Rectangle \r\n" + 
+				"2. Square \r\n" + 
+				"3. Circle \r\n" + 
+				"4. Hexagon \r\n" + 
+				"Area Calculator --- Choose your shape \r\n" + 
+				"");
+		int choice=sc.nextInt();
+		if(choice==1) {
+			System.out.println("Enter length and breadth: ");
+			int length=sc.nextInt();
+			int breadth=sc.nextInt();
+			Rectangle2 r=new Rectangle2(length, breadth);
+			System.out.println("Area of Rectangle is: "+Math.round(r.calculateArea()* 100.0)/100.0);
+		}else if(choice==2) {
+			System.out.println("Enter side: ");
+			int side=sc.nextInt();
+			Square2 s=new Square2(side);
+			System.out.println("Area of Square is: "+Math.round(s.calculateArea()* 100.0)/100.0);
+		}else if(choice==3){
+			System.out.println("Enter Radius: ");
+			int radius=sc.nextInt();
+			Circle2 c=new Circle2(radius);
+			System.out.println("Area of Circle is: "+Math.round(c.calculateArea()* 100.0)/100.0);
+		
+		}else {
+			System.out.println("Enter side: ");
+			int side=sc.nextInt();
+			System.out.println("Area of Hexagon is: 0.00");
+		}
+		sc.close();
+
+
+	}
+
+}
+class Shape2{
+	protected String shapeName; 
+	double calculateArea() {
+		return 0;
+	}
+	public Shape2(String shapeName) {
+		this.shapeName = shapeName;
+	}
+}
+class Square2 extends Shape2{
+	int side;
+	double calculateArea() {
+		return side*side;
+	}
+	public Square2( int side) {
+		super("Square");
+		this.side = side;
+	}
+	public int getSide() {
+		return side;
+	}
+	public void setSide(int side) {
+		this.side = side;
+	}
+	
+	
+}
+class Rectangle2 extends Shape2{
+	int length;
+	int breadth;
+	double calculateArea() {
+		return length*breadth;
+	}
+	public Rectangle2(int length, int breadth) {
+		super("Rectangle");
+		this.length = length;
+		this.breadth = breadth;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getBreadth() {
+		return breadth;
+	}
+	public void setBreadth(int breadth) {
+		this.breadth = breadth;
+	}
+	
+	
+}
+class Circle2 extends Shape2{
+	int radius;
+	double calculateArea() {
+		return Math.PI*(radius*radius);
+	}
+	public Circle2( int radius) {
+		super("Circle");
+		this.radius = radius;
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	
+	
+}
+
+class Hexagon extends Shape2{
+	int side;
+
+	public Hexagon( int side) {
+		super("Hexagon");
+		this.side = side;
+	}
+	
+}
--- src/day7/EmiMonthCalculator.java
+++ src/day7/EmiMonthCalculator.java
@@ -1,5 +1,6 @@
 package day7;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class EmiMonthCalculator {
@@ -7,7 +8,7 @@ public class EmiMonthCalculator {
 	public static void main(String[] args)  {
 		Scanner sc=new Scanner(System.in);
 		String d= sc.nextLine();
-		UserMainCode.displayDate(d);
+		UserMainCode2.displayDate(d);
 		sc.close();
 		
 
@@ -16,18 +17,19 @@ public class EmiMonthCalculator {
 }
 class UserMainCode2 {
 	public static void displayDate(String date){
+		
 		try { 
-			String x[] = date.split("-");
-			for(String y:x){
-				System.out.println(y);
-			}
-			//int year=Integer.parseInt(x[0]);
-			//int month=Integer.parseInt(x[1]);
-			//int dat=Integer.parseInt(x[2]);
-			//System.out.println(year);
-			//LocalDate a = LocalDate.of(year,month,dat);
-			//LocalDate b = a.minusMonths(20);
-			//System.out.println("20 months before "+date+" will be "+b);
+			String x[] = date.split("\\-");
+			
+			int year=Integer.parseInt(x[0]);
+			int month=Integer.parseInt(x[1]);
+			int dat=Integer.parseInt(x[2]);
+			
+			LocalDate a = LocalDate.of(year,month,dat);
+			LocalDate b = a.minusMonths(20);
+			
+			System.out.println("20 months before "+date+" will be "+b);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
